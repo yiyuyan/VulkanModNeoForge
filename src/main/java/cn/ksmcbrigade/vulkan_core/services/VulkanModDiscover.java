cbf6fa280d2cc00d470a117a6134c54d9e7f2105
yiyuyan <179544143@qq.com>
2026-05-10 08:57:12 +0800

    move VulkanTransformationService.java into VulkanModDiscover

--- src/main/java/cn/ksmcbrigade/vulkan_core/services/VulkanModDiscover.java
+++ src/main/java/cn/ksmcbrigade/vulkan_core/services/VulkanModDiscover.java
@@ -2,6 +2,7 @@ package cn.ksmcbrigade.vulkan_core.services;
 
 import cn.ksmcbrigade.vulkan_core.VKCUnsafeUtils;
 import com.mojang.logging.LogUtils;
+import net.neoforged.fml.loading.FMLConfig;
 import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.fml.loading.LogMarkers;
 import net.neoforged.neoforgespi.ILaunchContext;
@@ -13,6 +14,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -25,6 +27,79 @@ import java.util.zip.ZipFile;
  * &#064;Date: 2025/9/16 下午7:06
  */
 public class VulkanModDiscover implements IModFileCandidateLocator {
+
+    static {
+        LogUtils.getLogger().info(LogMarkers.CORE,"VulkanTransformationService is Loading...");
+
+        final boolean earlyDisplay = FMLConfig.getBoolConfigValue(FMLConfig.ConfigValue.EARLY_WINDOW_CONTROL);
+
+        try {
+            FMLConfig.updateConfig(FMLConfig.ConfigValue.EARLY_WINDOW_CONTROL,false);
+            File file = new File("config/fml.toml");
+            FMLConfig.updateConfig(FMLConfig.ConfigValue.EARLY_WINDOW_CONTROL,false);
+            if(!file.exists()){
+                FileUtils.writeStringToFile(file, """
+                        #Early window height
+                        earlyWindowHeight = 480
+                        #Enable NeoForge global version checking
+                        versionCheck = true
+                        #Should we control the window. Disabling this disables new GL features and can be bad for mods that rely on them.
+                        earlyWindowControl = false
+                        #Early window framebuffer scale
+                        earlyWindowFBScale = 1
+                        #Disables File Watcher. Used to automatically update config if its file has been modified.
+                        disableConfigWatcher = false
+                        #Early window provider
+                        earlyWindowProvider = "fmlearlywindow"
+                        #Early window width
+                        earlyWindowWidth = 854
+                        #Early window starts maximized
+                        earlyWindowMaximized = false
+                        #Default config path for servers
+                        defaultConfigPath = "defaultconfigs"
+                        #Disables Optimized DFU client-side - already disabled on servers
+                        disableOptimizedDFU = true
+                        #Skip specific GL versions, may help with buggy graphics card drivers
+                        earlyWindowSkipGLVersions = []
+                        #Max threads for early initialization parallelism,  -1 is based on processor count
+                        maxThreads = -1
+                        #Squir?
+                        earlyWindowSquir = false
+                                            
+                        """);
+            }
+            else{
+                StringBuilder builder = new StringBuilder();
+                for (String string : FileUtils.readFileToString(file).split("\n")) {
+                    if(string.startsWith("earlyWindowControl")){
+                        builder.append(string.replace("true","false"));
+                    }
+                    else{
+                        builder.append(string);
+                    }
+                    builder.append("\n");
+                }
+                FileUtils.writeStringToFile(file,builder.toString());
+            }
+
+            if(earlyDisplay){
+                JOptionPane.showMessageDialog(
+                        null,
+                        "The earlyWindowControl has been turned off.\nPlease restart the game.",
+                        "VulkanModNeoForge",
+                        JOptionPane.WARNING_MESSAGE
+                );
+                LogUtils.getLogger().info(LogMarkers.CORE,"The earlyWindowControl has been turned.Please restart the game.");
+                LogUtils.getLogger().warn(LogMarkers.CORE,"Exiting...");
+                System.exit(0);
+            }
+
+            LogUtils.getLogger().info(LogMarkers.CORE,"VulkanTransformationService is Loaded.");
+        } catch (IOException e) {
+            LogUtils.getLogger().info(LogMarkers.CORE,"[VulkanCore] Can't close the early window control.");
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void findCandidates(ILaunchContext context, IDiscoveryPipeline pipeline) {
